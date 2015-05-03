be6828d4ea0818c3d13710e55efb6cc245ba216e
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-05-04 02:01:28 +0600

    Update iTerm install script
    Take care of cfprefsd daemon's caching stuff

--- iterm2/iterm2.sh
+++ iterm2/iterm2.sh
@@ -3,18 +3,17 @@
 cd "$(dirname "$0")/.."
 DOTFILES_ROOT=$(pwd)
 
-
-if [[ ! -d "/Applications/iTerm.app" ]];
-then
-	# Install the latest iTerm2 nightly build available
-	echo "• Installing iTerm2 (latest)"
-else
-	if [[ "iTerm.app" != "$TERM_PROGRAM" ]]; then
-		killall iTerm
-	fi
-fi
-
 echo "• Installing iTerm2 Preferences"
+# Kill iTerm (if it's not the current terminal) to avoid syncing problems
+if [[ "iTerm.app" != "$TERM_PROGRAM" ]]; then
+        killall iTerm
+fi
+# Kill the prefs deamon to avoid re-syncing of old preferences
+killall cfprefsd
 # Symlink com.googlecode.iterm2.plist
 ln -s -F -i "$DOTFILES_ROOT/iterm2/com.googlecode.iterm2.plist" \
             "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
+# Kill it again to force re-syncing of new preferences
+killall cfprefsd
+# Re-open iTerm.app
+open -a iTerm
