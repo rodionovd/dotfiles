2a1501f06f0ae6da56063c990f234f5330121793
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-05 12:47:55 +0600

    xcode.sh: get rid of fancy colors since everything is automated now, so we don't need a user's attention

--- xcode.sh
+++ xcode.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-PURPLE="\e[1;35m"
-CYAN="\e[4;36m"
-CLEAR="\e[0m"
-
-printf "• Installing Xcode command-line tools. ${PURPLE}Choose ${CYAN}\"Install\"${CLEAR}${PURPLE} in the pop-up window you'll see.${CLEAR}\n"
+printf "• Installing Xcode command-line tools.\n"
 xcode-select --install
 
 # Credits to Bryan Goldstein:
