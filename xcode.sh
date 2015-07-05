601381e50e9fb34cbd9e95c0c4c59be0bec090b9
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-05 12:42:57 +0600

    xcode.sh: automatically click buttons in pop-up windows
    Credits to Bryan Goldstein:  https://gist.github.com/brysgo/9007731

--- xcode.sh
+++ xcode.sh
@@ -6,3 +6,15 @@ CLEAR="\e[0m"
 
 printf "• Installing Xcode command-line tools. ${PURPLE}Choose ${CYAN}\"Install\"${CLEAR}${PURPLE} in the pop-up window you'll see.${CLEAR}\n"
 xcode-select --install
+
+# Credits to Bryan Goldstein:
+# see https://gist.github.com/brysgo/9007731
+sleep 1
+sudo osascript <<EOD
+  tell application "System Events"
+    tell process "Install Command Line Developer Tools"
+      keystroke return
+      click button "Agree" of window "License Agreement"
+    end tell
+  end tell
+EOD
