ff8f7255ae4be3b149fc32b6e423ec3a4d18f277
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-05 12:26:04 +0600

    Add installer for pip python packages

---
+++ pip.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install pip if nedded
+if [[ ! -f $(which pip) ]];
+then
+	echo "• Installing pip"
+    sudo easy_install pip
+fi
+
+echo "• Installing pip packages"
+for pkg in "machobot"; do
+	sudo pip install "${pkg}"
+done
