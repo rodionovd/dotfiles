6bb160be09e09531a096bb189c36221270b7a130
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-06 16:50:05 +0600

    cask/iterm2-nightly: update urls for https

--- cask/iterm2-nightly.rb
+++ cask/iterm2-nightly.rb
@@ -2,13 +2,13 @@ cask :v1 => 'iterm2-nightly' do
   version :latest
   sha256 :no_check
 
-  url 'http://iterm2.com/nightly/latest'
+  url 'https://www.iterm2.com/nightly/latest'
   name 'iTerm2'
   name 'iTerm 2'
-  homepage 'http://www.iterm2.com/'
-  appcast 'https://iterm2.com/appcasts/nightly.xml'
-
+  homepage 'https://www.iterm2.com/'
   license :gpl
+
+  appcast 'https://iterm2.com/appcasts/nightly.xml'
 
   app 'iTerm.app'
   zap :delete => [
