21135ce42f061a0e422f00c2512ccb371231bb31
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-10-09 22:32:58 +0600

    Don't install Xcode automatically

--- xcode.sh
+++ xcode.sh
@@ -18,17 +18,17 @@ EOD
 # Also install Xcode.app itself via `xcode-install` by Boris Bügling:
 # https://github.com/neonichu/xcode-install
 
-XCODES_TO_INSTALL=("6.4")
-
-USER=$(/usr/bin/security find-generic-password -wa "XCODE_INSTALL_USER")
-if [[ $? == 0 ]]; then
-    PASS=$(/usr/bin/security find-generic-password -wa "XCODE_INSTALL_PASSWORD")
-    if [[ $? == 0 ]]; then
-        sudo gem install --no-document xcode-install
-        for version in "${XCODES_TO_INSTALL[@]}"
-        do
-            XCODE_INSTALL_USER="$USER" XCODE_INSTALL_PASSWORD="$PASS" xcode-install install --verbose "$version"
-        done
-        xcode-install cleanup
-    fi
-fi
+# XCODES_TO_INSTALL=("6.4")
+#
+# USER=$(/usr/bin/security find-generic-password -wa "XCODE_INSTALL_USER")
+# if [[ $? == 0 ]]; then
+#     PASS=$(/usr/bin/security find-generic-password -wa "XCODE_INSTALL_PASSWORD")
+#     if [[ $? == 0 ]]; then
+#         sudo gem install --no-document xcode-install
+#         for version in "${XCODES_TO_INSTALL[@]}"
+#         do
+#             XCODE_INSTALL_USER="$USER" XCODE_INSTALL_PASSWORD="$PASS" xcode-install install --verbose "$version"
+#         done
+#         xcode-install cleanup
+#     fi
+# fi
