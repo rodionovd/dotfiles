d40858e947055d350249986c0052b216bdea3182
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-12-23 02:11:49 +0600

    OS X: Reduce Transparency

--- osx.sh
+++ osx.sh
@@ -45,6 +45,9 @@ defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
 # Click in the scroll bar: Jump to the spot that's clicked
 defaults write NSGlobalDomain AppleScrollerPagingBehavior -int 1
 
+# Reduce Transparency
+defaults write com.apple.universalaccess reduceTransparency -bool true
+
 # --[ Trackpad ]--
 
 # Enable tap to click
