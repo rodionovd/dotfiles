f5d8e52fe3418ace8327385a6d7a105389bbcc40
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-04-03 15:10:53 +0600

    brew+zsh: add rbenv stuff

--- brew.sh
+++ brew.sh
@@ -15,6 +15,6 @@ fi
 # Install the required stuff
 echo "• Installing Homebrew formulae"
 brew doctor
-for formulae in "git" "zsh" "sqlite" "cloc" "terminal-notifier" "pandoc" "tree" "sshuttle"; do
+for formulae in "git" "zsh" "sqlite" "cloc" "terminal-notifier" "pandoc" "tree" "sshuttle" "rbenv"; do
 	brew install "${formulae}"
 done
