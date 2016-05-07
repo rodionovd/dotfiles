ca0f0e09fcba9717af3178a4fbe2ebde85a5f93d
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-05-07 15:44:39 +0600

    Include .gitconfig

--- git/git.sh
+++ git/git.sh
@@ -15,4 +15,6 @@ git config --global user.email "$GIT_AUTHOR_EMAIL"
 
 echo "• Installing global .gitignore"
 ln -s -F -i "$DOTFILES_ROOT/git/gitignore_global.symlink" "$HOME/.gitignore_global"
-git config --global core.excludesfile "$HOME/.gitignore_global"
+
+echo "• Installing .gitconfig"
+ln -s -F -i "$DOTFILES_ROOT/git/gitconfig.symlink" "$HOME/.gitconfig"
