c262659978fd7d123ef2c9656b05a205b0810628
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-09-05 16:04:52 +0700

    Cask: install RB App Checker Lite

--- cask/cask.sh
+++ cask/cask.sh
@@ -8,7 +8,7 @@ brew doctor
 # Install some OS X apps using Cask
 echo "• Installing Homebrew Cask and some apps"
 brew install caskroom/cask/brew-cask
-for app in "github-desktop" "hopper-disassembler" "skype" "the-unarchiver" "transmission" "sketch" "coderunner" "slack" "0xed" "virtualbox" "dropdmg" "atom" "pacifist" "dash" "wwdc" "marked" "vlc" "hammerspoon" "firefox" "paw" "hex-fiend" "gpgtools"; do
+for app in "github-desktop" "hopper-disassembler" "skype" "the-unarchiver" "transmission" "sketch" "coderunner" "slack" "0xed" "virtualbox" "dropdmg" "atom" "pacifist" "dash" "wwdc" "marked" "vlc" "hammerspoon" "firefox" "paw" "hex-fiend" "gpgtools" "rb-app-checker-lite"; do
 	brew cask install "${app}"
 done
 
