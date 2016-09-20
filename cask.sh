feb46bed4745f0012923655c3b6b318b192528c9
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2016-09-20 11:09:01 +0700

    Install stable version of iTerm2 instead of a nightly build

--- cask.sh
+++ cask.sh
@@ -5,16 +5,8 @@ brew doctor
 # Install some OS X apps using Cask
 echo "• Installing Homebrew Cask and some apps"
 brew install caskroom/cask/brew-cask
-for app in "github-desktop" "hopper-disassembler" "skype" "the-unarchiver" "transmission" "sketch" "coderunner" "slack" "0xed" "virtualbox" "dropdmg" "atom" "pacifist" "dash" "wwdc" "marked" "vlc" "hammerspoon" "firefox" "paw" "hex-fiend" "gpgtools" "rb-app-checker-lite"; do
+for app in "iterm2" "github-desktop" "hopper-disassembler" "skype" "the-unarchiver" "transmission" "sketch" "coderunner" "slack" "0xed" "virtualbox" "dropdmg" "atom" "pacifist" "dash" "wwdc" "marked" "vlc" "hammerspoon" "firefox" "paw" "hex-fiend" "gpgtools" "rb-app-checker-lite"; do
 	brew cask install "${app}"
 done
 
-# Also install the latest iTerm2 nightly build (if needed)
-if [[ ! -d "/Applications/iTerm.app" ]];
-then
-	echo "• Installing iTerm2 (latest)"
-	brew tap caskroom/versions
-	brew cask install iterm2-nightly
-	brew untap caskroom/versions
-fi
 brew cask cleanup
