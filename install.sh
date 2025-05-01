0356586629b732f6331c4c4e90de18a0150a538f
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2025-05-01 15:08:11 +0400

    install.sh: brew bundle may fail to install some casks. Swallow those errors and continue

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo "=================================="
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/opt/homebrew/bin/brew shellenv)"
-brew bundle install --file "$ROOT/brew/Brewfile"
+brew bundle install --file "$ROOT/brew/Brewfile" || true
 
 echo "=================================="
 echo "Step 3: Configuring git & ssh"
